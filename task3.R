9f78de7ad9c7ed8ef7660c1d7fdea6efea99cf73
Sergiu Petru Lupescu <sergiu_lupescu@hotmail.com>
2019-02-13 17:09:13 +0100

    Correlation Matrix

--- task3.R
+++ task3.R
@@ -1,6 +1,7 @@
 # library(readr)
 # library(ggplot2)
 # library(caret)
+# library(corrplot)
 
 # load dataset.
 ex_product <- read.csv("existingproductattributes2017.2.csv")
@@ -10,7 +11,37 @@ new_product <- read.csv("newproductattributes2017.2.csv")
 summary(ex_product)
 summary(new_product)
 
-# prepare data.
-dmy_existing <- dummyVars(" ~.", ex_product)
-ready_ex_product <- data.frame(predict(dmy_existing, ex_product))
+# plot histogram.
+ggplot(ex_product, aes(x = Volume)) + 
+  geom_histogram(colour = "darkblue", fill = "lightblue")
 
+# volume outliers and the row of the outliers.
+volume_outlier <- boxplot.stats(ex_product$Volume)$out
+volume_out_rows <- which(ex_product$Volume %in% volume_outlier)
+
+# filter the outliers.
+ex_prod_ready <- filter(ex_product, Volume < 7000)
+
+# prepare data.
+dmy_ex_index <- dummyVars(" ~.", ex_prod_ready)
+ex_prod_dummy <- data.frame(predict(dmy_ex_index, ex_prod_ready))
+
+# remove features.
+ex_prod_rmv <- ex_prod_dummy
+ex_prod_rmv$ProductNum = NULL
+ex_prod_rmv$BestSellersRank = NULL
+ex_prod_rmv$ProductType.ExtendedWarranty = NULL
+
+
+# correlation matrix.
+cor_rmv <- cor(ex_prod_rmv)
+cor_rmv
+corrplot(cor_rmv)
+
+colnames(ex_prod_rmv) <- c("PT_A", "PT_D", "PT_GC", "PT_L", 
+                           "PT_N", "PT_PC", "PT_P", "PT_PS", 
+                           "PT_SM", "PT_S", "PT_T", "Price", 
+                           "5star", "4star", "3star", "2star",
+                           "1star", "PosR", "NegR", "RecP", 
+                           "ShipW", "ProD", "ProW", "ProH", 
+                           "ProfM", "Vol")
